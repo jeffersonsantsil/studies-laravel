987f34f1c367a26cf107ea231f3c24bf4c12c02f
Jefferson Silva <jeffersonsantsil@gmail.com>
2024-01-29 16:07:44 -0300

    WIP

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,18 @@
 <?php
 
-use App\Http\Controllers\{
-    ComponetesController,
-    FraldasController,
-    UserController
-};
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-Route::get('/user/{id}/edit', [UserController:: class, 'edit'])->name('user.edit');
-//Route::get('/', [UserController::class, 'index'])->name('index');
-Route::get('/user/{id}', [UserController::class, 'show'])->name('user.show');
-Route::post('/cadUser', [UserController::class, 'createUser'])->name('cad.user');
-
-
-Route::get('/', [FraldasController::class, 'indexLogin'])->name('fraldas.indexLogin');
-Route::post('/', [FraldasController::class, 'login'])->name('fraldas.login');
-Route::get('/peso-fralda',[FraldasController:: class, 'pesoFraldas'])->name('fraldas.peso');
-Route::post('/cadFralda', [FraldasController::class, 'cadFralda'])->name('cad.fralda');
-
-
+Route::get('/', function () {
+    return view('site.index');
+});
